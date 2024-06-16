a22c2f9ba0a5efb4072a6318f26c849e9d5e3de2
KoksharovSA <koksharovsa@gmail.com>
2024-06-16 14:21:19 +0700

    Создано SpringBoot приложение.

---
+++ JenkinsSirvice/src/main/java/ru/konsist/JenkinsService/JenkinsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.konsist.JenkinsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsServiceApplication.class, args);
+	}
+
+}
